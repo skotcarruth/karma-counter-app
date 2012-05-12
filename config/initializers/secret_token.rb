7e636638d577b746fd1102a24eddeb0a9d6e3a3a
Skot Carruth <skot@gophilosophie.com>
2012-05-11 22:55:55 -0700

    new rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KarmaCounterApp::Application.config.secret_token = '6adc14368aef536ac6714dde3ae41c92c80126a46ea2b637213bd5e8b67d35b179c2cc0a2b6306ccaf0df3ccdd87575d233d4661c9d953b96ced4d13670499e1'
